7e6db7e5394877de0bed2cb2d7b7d79c8a53e62e
mhkeller <michael.keller@gmail.com>
2015-07-03 01:49:44 -0400

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+var newslynx = require('newslynx');
+
+const app = require('app');
+const BrowserWindow = require('browser-window');
+
+// report crashes to the Electron project
+require('crash-reporter').start();
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+function createMainWindow () {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 400,
+		resizable: false
+	});
+
+	win.loadUrl(`http://localhost:3000`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+function onClosed() {
+	// deref the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+// prevent window being GC'd
+let mainWindow;
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate-with-no-open-windows', function () {
+	if (!mainWindow) {
+		newslynx.run();
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', function () {
+	mainWindow = createMainWindow();
+});
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Newslynx Electron
+
+A desktop version of [NewsLynx app](http://github.com/newslynx/newslynx-app) running on [Electron](https://github.com/atom/electron). Scaffolded with [generator-electron](https://github.com/sindresorhus/generator-electron). Still a work in progress. To be released August 2015. Check the [project issue tracker(http://github.com/newslynx/opportunities)for updates.
+
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+
+## License
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
